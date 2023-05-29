57eca4bc502c5d708339dc844b8274b107850d5a
Aidos <aidos.zhubandyk@gmail.com>
2023-05-29 16:09:38 +0600

    first commit

---
+++ task1/app.js
@@ -0,0 +1,17 @@
+
+
+  const app = Vue.createApp({
+    data() {
+        return {
+            name: '',
+            surname: '',
+            fathername: '',
+            country: '',
+            excursion: [],
+            baggage: '',
+            comments: '',
+        }
+    }
+  })
+
+  app.mount('#app')
